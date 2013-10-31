b1b43ff75cfdbabb766eb00e58730c361ce70d47
andy fang <andy@mingtak.com.tw>
2013-10-31 17:31:53 +0000

    新增programSet, 將程式碼放在這裏集中管理

--- babywithme.content/babywithme/content/programset.py
+++ babywithme.content/babywithme/content/programset.py
@@ -1,6 +1,21 @@
 #-*- coding:utf-8 -*-
-from five import grok
+#from random import randrange
+#from datetime import datetime
+#import pickle
+import os
+from plone import api
+from Products.ATContentTypes.lib import constraintypes
+from Products.DCWorkflow.interfaces import *
+#使用archetype content type做為相關的觸發條件
+from Products.ATContentTypes.interfaces import folder
+from Products.Archetypes.interfaces import *
 
+from Products.PluggableAuthService.interfaces import events
+from zope.lifecycleevent.interfaces import IObjectAddedEvent
+
+#-------分隔線，以上是自已 import----------------------
+
+from five import grok
 from z3c.form import group, field
 from zope import schema
 from zope.interface import invariant, Invalid
@@ -20,17 +35,17 @@ from babywithme.content import MessageFactory as _
 
 # Interface class; used to define content-type schema.
 
-class IAlbum(form.Schema, IImageScaleTraversable):
+class IprogramSet(form.Schema, IImageScaleTraversable):
     """
-    Album for user
+    program set for babywithme
     """
 
     # If you want a schema-defined interface, delete the model.load
     # line below and delete the matching file in the models sub-directory.
     # If you want a model-based interface, edit
-    # models/album.xml to define the content type.
+    # models/programset.xml to define the content type.
 
-    form.model("models/album.xml")
+    form.model("models/programset.xml")
 
 
 # Custom content-type class; objects created for this content type will
@@ -38,15 +53,15 @@ class IAlbum(form.Schema, IImageScaleTraversable):
 # methods and properties. Put methods that are mainly useful for rendering
 # in separate view classes.
 
-class Album(Container):
-    grok.implements(IAlbum)
+class programSet(Container):
+    grok.implements(IprogramSet)
 
     # Add your class methods and properties here
 
 
 # View class
 # The view will automatically use a similarly named template in
-# album_templates.
+# programset_templates.
 # Template filenames should be all lower case.
 # The view will render when you request a content object with this
 # interface with "/@@sampleview" appended.
@@ -57,9 +72,27 @@ class Album(Container):
 class SampleView(grok.View):
     """ sample view class """
 
-    grok.context(IAlbum)
+    grok.context(IprogramSet)
     grok.require('zope2.View')
 
     # grok.name('view')
 
     # Add view methods here
+
+
+#-------------以下自定義程式------------------------
+
+
+#新註冊id,系統會開使用者家目錄，初始化這個目錄
+@grok.subscribe(folder.IATFolder, IObjectAddedEvent)
+def changeState(content, event):
+    portal = api.portal.get()
+    id = str(content.getId())
+    uid = str(content.UID())
+    if portal['Members'].has_key(id) and portal['Members'][id].UID() == uid:
+        userHomeDir = portal['Members'][id]
+        with open ('/home/plone/yyyyy','a') as yyyyy:
+            yyyyy.write('HI, Program Set!')
+
+    else:
+        return
