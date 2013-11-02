ea62918c863fd01622748346ecfc96365819a2c9
andy fang <andy@mingtak.com.tw>
2013-11-02 02:43:39 +0000

    workflow完成

--- babywithme.content/babywithme/content/programset.py
+++ babywithme.content/babywithme/content/programset.py
@@ -18,6 +18,63 @@ from datetime import datetime
 from random import randrange
 from Products.ATContentTypes.lib import constraintypes
 
+
+#寫入測試，寫到/home/plone/yyyyy
+def writeinTest(string='please give me a text string'):
+    with open('/home/plone/yyyyy', 'a') as yyyyy:
+        yyyyy.write(string + '\n')
+
+
+
+#切換blog的workflow狀態
+def changeBlogState(blogFolder, new_state):
+    catalog = api.portal.get_tool(name='portal_catalog')
+    ownerId= str(blogFolder.getOwner())
+    if new_state == 'published':
+        searchState = 'tempStat'
+    elif new_state == 'private':
+        searchState = 'published'
+    items = catalog.searchResults({'Creator':ownerId,
+                                   'review_state':searchState,
+                                   'portal_type':['babywithme.content.album', 
+                                                  'babywithme.content.playgroup', 
+                                                  'Document']})
+    writeinTest(str(type(items)))
+    writeinTest(str(len(items)))
+    if len(items) == 0:
+        return
+
+    writeinTest('len(items)>0')
+
+    if new_state == 'private':
+        for item in items:
+            contentObject = api.content.get(UID=item.UID)
+            api.content.transition(obj=contentObject, transition="toTempStat")
+            contentObject.reindexObject()
+    elif new_state == 'published':
+        for item in items:
+            contentObject = api.content.get(UID=item.UID)
+            api.content.transition(obj=contentObject, transition="publish")
+            contentObject.reindexObject()
+
+
+
+#部落格關閉時，將該部落格下面所有published的content改為tmpStat,
+#部落格重新啟動後，tmpStat再回到published
+@grok.subscribe(folder.IATFolder, IAfterTransitionEvent)
+def blogCloseOrReopen(blog, event):
+    portal = api.portal.get()
+    id = str(blog.getId())
+    uid = str(blog.UID())
+
+    #確認這個目錄是個部落格，而不是其他的，才執行
+    if portal['Members'].has_key(id) and portal['Members'][id].UID() == uid:
+        writeinTest('有call changeBlogState')
+        changeBlogState(blogFolder=blog, new_state=event.new_state.id)
+
+
+
+
 ''' 以下暫擱置
 #變更id,格式：yyyyMMddhhmm + random(3)
 def changeContentId(content):
