cd03f463f39ad94cc0b37961004c2da85e225d13
andy fang <andy@mingtak.com.tw>
2013-11-05 06:03:13 +0000

    View設計開始

--- babywithme.content/babywithme/content/playgroup.py
+++ babywithme.content/babywithme/content/playgroup.py
@@ -53,7 +53,7 @@ class Playgroup(Container):
 # of this type by uncommenting the grok.name line below or by
 # changing the view class name and template filename to View / view.pt.
 
-class SampleView(grok.View):
+class View(grok.View):
     """ sample view class """
 
     grok.context(IPlaygroup)
